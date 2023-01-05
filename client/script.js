cf763023cbf4f23732197743e0878111f97d1df1
imshambles <vishwas.latiyan007@gmail.com>
2023-01-05 18:43:37 +0530

    changing server url

--- client/script.js
+++ client/script.js
@@ -72,7 +72,7 @@ const handleSubmit = async(e) => {
 
   // fetch from server -> bot's response 
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://friday-0q6s.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
